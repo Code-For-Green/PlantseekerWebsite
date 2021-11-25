d43465b198d61088ee30c83d0361804b66ed8e9b
PanSzelescik <panszelescik@gmail.com>
2021-11-25 13:42:28 +0100

    Działa, zapisuje pliki

--- panel/add_request.php
+++ panel/add_request.php
@@ -2,10 +2,11 @@
 
 $name = $_POST['name'];
 $description = $_POST['description'];
-$file = $_POST['file'];
+$file = $_FILES['file'];
 
-echo $name;
-echo '<br>';
-echo $description;
-echo '<br>';
-echo $file;
+$uploaddir = './';
+$uploadfile = $uploaddir . basename($file['name']);
+
+move_uploaded_file($file['tmp_name'], $uploadfile);
+
+echo $name . ' : '. $description;
