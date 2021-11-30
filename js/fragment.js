8d79ffab01efc458ff35790d37481bc6846d799a
PanSzelescik <panszelescik@gmail.com>
2021-11-30 14:36:10 +0100

    active class

--- js/fragment.js
+++ js/fragment.js
@@ -1,4 +1,4 @@
-function loadFragment(name = 'dashboard') {
+function loadFragment(name = 'dashboard', onThen = () => {}) {
     fetch(`./fragments/${name}.html`)
         .then((response) => {
             if (!response.ok) {
@@ -12,13 +12,17 @@ function loadFragment(name = 'dashboard') {
             document.getElementById('main-content').innerHTML = html;
             updateCharts();
         })
+        .then(onThen)
         .catch(console.error);
 }
 
 loadFragment();
 
-[...document.getElementsByClassName('link-content')].forEach((element) => {
+[...document.getElementsByClassName('link-content')].forEach((element, _, elements) => {
     element.addEventListener('click', () => {
-        loadFragment(element.dataset.fragment);
+        loadFragment(element.dataset.fragment, () => {
+            element.classList.add('active');
+            elements.forEach((e) => e.classList.remove('active'));
+        });
     });
 });
